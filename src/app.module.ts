6cf44573fbc2fd07638f75773c273c6163ca8dae
muslimbek-sotvoldiyev <muslimbeksotvoldiyev581@gmail.com>
2026-07-05 04:17:32 -0700

    wallet qoshildi

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
+import { WalletModule } from './wallet/wallet.module';
 
 @Module({
   imports: [
@@ -32,6 +33,8 @@ import { ProductModule } from './product/product.module';
     }),
 
     ProductModule,
+
+    WalletModule,
 
     
 
